0be4b8ea8da1feeedf289c3aa72721f87e6c34e2
wesley-foussard <53617379+nimiteos-it@users.noreply.github.com>
2022-10-13 23:11:16 +0200

    tests

---
+++ js/app.js
@@ -0,0 +1,49 @@
+//générateur de nombre aléatoire
+function getRandomNumber(min = 1, max = 100) {
+  const randomNumber = Math.round(Math.random() * (max - min) + min);
+
+  return randomNumber;
+}
+/*--------DEALER------------*/
+// je crée une div pour le plateau
+const plateauDealerDiv = document.createElement("div");
+plateauDealerDiv.setAttribute("id", "dealer");
+plateauDealerDiv.className = "board";
+app.appendChild(plateauDealerDiv);
+
+function dice() {
+  /*------------PLAYER-----------*/
+  // je crée une div pour le dé
+  const diceDiv = document.createElement("div");
+  // je lui donne la classe dice
+  diceDiv.className = "dice";
+  // j'isole la div player (pas nécéssaire vraisemblablement)
+  //   const player = document.getElementById("player");
+  // je lui ajoute la div `dice`
+  player.appendChild(diceDiv);
+
+  /*------------DEALER-----------*/
+  //   // je crée une div pour le dé
+  //   const dealerDiv = document.createElement("div");
+  //   // je lui donne la classe dice
+  //   dealerDiv.className = "dice";
+  //   // j'isole la div player
+  //   const dealer = document.getElementById("dealer");
+  //   // je lui ajoute la div `dice`
+  //   plateauDealerDiv.appendChild(dealer);
+
+  /*---------DICE--------------- */
+  //on génère le tirage du dé
+  let diceValue = getRandomNumber(1, 6);
+  // console.log(diceValue);
+  //on change la position du sprite en fonction du tirage du dé
+  let dicePosX = -(diceValue * 100 - 100);
+  // console.log(dicePosX);
+  diceDiv.style.backgroundPositionX = `${dicePosX}px`;
+}
+/* Main*/
+let diceThrows = parseInt(prompt("Combien de dés voulez-vous lancer"));
+
+for (i = 0; i < diceThrows; i++) {
+  dice();
+}
