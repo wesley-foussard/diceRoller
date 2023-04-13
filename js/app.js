2f403790bb0b98b8589d5d532436195985b02918
wesley-foussard <53617379+nimiteos-it@users.noreply.github.com>
2023-04-13 16:06:21 +0200

    editing code and cleanup

--- js/app.js
+++ js/app.js
@@ -1,47 +1,35 @@
-//générateur de nombre aléatoire
-function getRandomNumber(min = 1, max = 100) {
-  const randomNumber = Math.round(Math.random() * (max - min) + min);
-
-  return randomNumber;
+// Fonction récupérer sur MDN
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#getting_a_random_integer_between_two_values_inclusive
+function getRandomNumber(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
 }
-/*--------DEALER------------*/
-// je crée une div pour le plateau
-const plateauDealerDiv = document.createElement("div");
-plateauDealerDiv.setAttribute("id", "dealer");
-plateauDealerDiv.className = "board";
-app.appendChild(plateauDealerDiv);
 
-function dice() {
-  /*------------PLAYER-----------*/
-  // je crée une div pour le dé
-  const diceDiv = document.createElement("div");
-  // je lui donne la classe dice
-  diceDiv.className =
-    "dice"; /* className écrase toutes les autres classes, préférer classList.add
-  // j'isole la div player (pas nécéssaire vraisemblablement)
-  //   const player = document.getElementById("player");
-  // je lui ajoute la div `dice`
-  player.appendChild(diceDiv);
+// J'ai pris toutes les instructions pour la création d'un dé et je les ai mises dans une fonction
+function createDice(boardId) {
+  // Je créer ma div
+  const diceElm = document.createElement("div");
 
-  /*------------DEALER-----------*/
-  //   // je crée une div pour le dé
-  //   const dealerDiv = document.createElement("div");
-  //   // je lui donne la classe dice
-  //   dealerDiv.className = "dice";
-  //   // j'isole la div player
-  //   const dealer = document.getElementById("dealer");
-  //   // je lui ajoute la div `dice`
-  //   plateauDealerDiv.appendChild(dealer);
+  // Ajouter la class dice.
+  // classList est un objet contenant des méthodes me permettant d'intéragir avec les class de mon élément HTML.
+  diceElm.classList.add("dice");
 
-  /*---------DICE--------------- */
-  //on génère le tirage du dé
-  let diceValue = getRandomNumber(1, 6); //ici /!\
-  // console.log(diceValue); /* on peut jouer avec debugger au lieu de console.log, plus souple pour manipuler le code
-  //on change la position du sprite en fonction du tirage du dé
-  let dicePosX = -(diceValue * 100 - 100);
-  // console.log(dicePosX);
-  diceDiv.style.backgroundPositionX = `${dicePosX}px`; //-(${diceValue} * 100 - 100)`
+  // On va rajouter notre élément dans la div ayant l'identifiant player
+  // On récupère l'endroit où ajouter mon élément
+  const playerElm = document.getElementById(boardId);
+  // On ajouter dans mon élément récupérer le dé
+  // appendChild === append
+  playerElm.append(diceElm);
+
+  // Je tire mon nombre aléatoire
+  const diceNumber = getRandomNumber(1, 6);
+  // On souhaite avoir la position de notre image entre 0 et -500px. 0 étant la face de dé 1 et -500 la face de dé 6
+  const diceBackgroundPositionX = (diceNumber - 1) * 100 * -1;
+  // Je modifie la propriété de mon élément dé
+  diceElm.style.backgroundPositionX = `${diceBackgroundPositionX}px`;
 }
+
 function createPlayer() {
   const playerElm = document.createElement("div");
   // On doit ajouter une class et un id
@@ -54,10 +42,19 @@ function createPlayer() {
 }
 
 createPlayer();
-/* Main*/
 
-let diceThrows = parseInt(prompt("Combien de dés voulez-vous lancer"));
+// On pose la question à l'utilisateur sur le nombre de dés que l'on transforme tout de suite en nombre
+const nbOfDice = Number(prompt("Choisissez le nombre de dés"));
 
-for (i = 0; i < diceThrows; i++) {
-  dice(player);
+// Je lance autant de dés que l'utilisateur ma demandé
+// une boucle for va
+// 1 - initialisation ma variable
+// 2 - vérifier la condition
+//     - Si la condition est vrai on execute le code dans la boucle
+//     - Sinon on passe a la suite
+// 3 - on appel notre 3ème instruction qui est d'incrémenter notre variable => On repart à l'étape 2
+for (let nbDice = 0; nbDice < nbOfDice; nbDice++) {
+  // Lors de l'appel à notre fonction de création de dé, on passe en argument l'identifiant
+  createDice("player");
+  createDice("dealer");
 }
